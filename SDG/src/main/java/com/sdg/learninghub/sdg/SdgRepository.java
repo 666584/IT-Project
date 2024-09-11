83c298a6a9cac5d04419a90142c8db1093cec96e
Sussie <yurimc@student.unimelb.edu.au>
2024-09-12 01:08:53 +1000

    Fixed error
    - Fixed small errors on search bar implementation.

--- SDG/src/main/java/com/sdg/learninghub/sdg/SdgRepository.java
+++ SDG/src/main/java/com/sdg/learninghub/sdg/SdgRepository.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface SdgRepository extends JpaRepository<Sdg, Long>{
+@Repository
+public interface SdgRepository extends JpaRepository<Sdg, Integer>{
     @Query("SELECT s From Sdg s WHERE CONCAT(s.id, '',s.title, ' ', s.content, ' ') LIKE %?1%")
     List<Sdg> findAll(String keyword);
 }
