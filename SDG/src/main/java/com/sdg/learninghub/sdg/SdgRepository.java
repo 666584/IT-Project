6777fb1d29fb6456f50b7b334f48094b219fb92e
Viv <weda@student.unimelb.edu.au>
2024-09-14 21:54:02 +1000

    Varied attribute name (goal_id)

--- SDG/src/main/java/com/sdg/learninghub/sdg/SdgRepository.java
+++ SDG/src/main/java/com/sdg/learninghub/sdg/SdgRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SdgRepository extends JpaRepository<Sdg, Integer>{
-    @Query("SELECT s From Sdg s WHERE CONCAT(s.id, '',s.title, ' ', s.content, ' ') LIKE %?1%")
+    @Query("SELECT s From Sdg s WHERE CONCAT(s.goal_id, '',s.title, ' ', s.content, ' ') LIKE %?1%")
     List<Sdg> search(String keyword);
 }
