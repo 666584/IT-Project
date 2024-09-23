52b9d949437f7677a5dfb01dadb558e01da27f33
Viv <weda@student.unimelb.edu.au>
2024-09-23 17:45:56 +1000

    Finalise the search bar functionality, changed type of id for Sdg

--- SDG/src/main/java/com/sdg/learninghub/sdg/SdgRepository.java
+++ SDG/src/main/java/com/sdg/learninghub/sdg/SdgRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface SdgRepository extends JpaRepository<Sdg, Long>{
+public interface SdgRepository extends JpaRepository<Sdg, Integer>{
     @Query("SELECT s From Sdg s WHERE CONCAT(s.goal_id, '',s.title, ' ', s.content, ' ') LIKE %?1%")
     List<Sdg> search(String keyword);
 }
