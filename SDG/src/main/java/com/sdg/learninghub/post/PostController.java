22547e180ebc57eb22fedf5687301e697d5ac9a5
Viv <weda@student.unimelb.edu.au>
2024-10-15 10:47:23 +1100

    Implemented the view count and like count functionality of the posts

--- SDG/src/main/java/com/sdg/learninghub/post/PostController.java
+++ SDG/src/main/java/com/sdg/learninghub/post/PostController.java
@@ -1,13 +1,13 @@
 package com.sdg.learninghub.post;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,5 +42,25 @@ public class PostController {
     public String deletePost(@PathVariable(name = "id") int id) {
         postService.delete(id);
         return "redirect:/post/";
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> getPostById(@PathVariable Integer postId){
+        try{
+            Post post = postService.getPostByID(postId);
+            return ResponseEntity.ok(post);
+        } catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @RequestMapping("/{postId}/like")
+    public ResponseEntity<?> likePost(@PathVariable Integer postId){
+        try{
+            postService.likePost(postId);
+            return ResponseEntity.ok(new String[]{"Post liked successfully."});
+        } catch (EntityNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 }
--- SDG/src/main/java/com/sdg/learninghub/post/PostService.java
+++ SDG/src/main/java/com/sdg/learninghub/post/PostService.java
@@ -1,10 +1,12 @@
 package com.sdg.learninghub.post;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -15,11 +17,36 @@ public class PostService {
         return postRepository.findAll();
     }
 
+    /** save the new created post*/
     public void save(Post post) {
         post.setLikeCount(0);
         post.setViewCount(0);
         post.setLastUpdate(new Date());
         postRepository.save(post);
+    }
+
+    /** Count the views of the post*/
+    public Post getPostByID(Integer postId){
+        Optional<Post> optionalPost = postRepository.findById(postId);
+        if (optionalPost.isPresent()){
+            Post post = optionalPost.get();
+            post.setViewCount(post.getViewCount() + 1);
+            return postRepository.save(post);
+        } else{
+            throw new EntityNotFoundException("Post not found");
+        }
+    }
+
+    /** Count the likes of the post*/
+    public void likePost(Integer postId){
+        Optional<Post> optionalPost = postRepository.findById(postId);
+        if (optionalPost.isPresent()){
+            Post post = optionalPost.get();
+            post.setLikeCount(post.getLikeCount() + 1);
+            postRepository.save(post);
+        } else {
+            throw new EntityNotFoundException("Post not found with id: " + postId);
+        }
     }
 
     public void delete(int id) {
