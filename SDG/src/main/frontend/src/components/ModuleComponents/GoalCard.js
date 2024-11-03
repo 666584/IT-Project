f3b310949fdb45afa01614da3df727bb9ca6e188
Sussie <yurimc@student.unimelb.edu.au>
2024-11-03 18:05:58 +1100

    Fixing file name error

---
+++ SDG/src/main/frontend/src/components/ModuleComponents/GoalCard.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './GoalCard.css';
+import playbutton from '../../assets/GoalAssets/playbutton.svg';
+import { useNavigate } from 'react-router-dom';
+import AuthAPI from '../../services/AuthAPI.js';
+
+function GoalCard({ title, goal, color, icon }) {
+  const navigate = useNavigate();
+  const [message, setMessage] = useState('');
+
+  const handleClick = async(e) => {
+    e.preventDefault();
+    const accessToken = localStorage.getItem('accessToken');                  
+    try {
+      const response = await AuthAPI.auth({ accessToken });         
+      navigate(`/goal/${title}`);    
+    } catch (error) {
+      setMessage('Invalid credentials');
+      console.log(message);
+    }
+  };
+
+  return (
+    <div className="goal-card" style={{ backgroundColor: color }}>
+      <div className="goal-icon">{icon}</div>
+      <button className="play-button" onClick={handleClick}>
+        <div>
+          <h3 className='titleti'>{title}</h3>
+          <p className='goalti'>{goal}</p>
+        </div>
+        <img src={playbutton} alt="Play" />
+      </button>
+    </div>
+  );
+}
+
+export default GoalCard;
