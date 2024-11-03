f353522365f74fd8153263b030e0c2584959d40c
Sussie <yurimc@student.unimelb.edu.au>
2024-11-03 19:55:39 +1100

    Update SecurityConfig.java

--- SDG/src/main/java/com/sdg/learninghub/SecurityConfig.java
+++ SDG/src/main/java/com/sdg/learninghub/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig implements WebMvcConfigurer{
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://sdglearninghub.web.app/", "https://sdglearninghub.firebaseapp.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
